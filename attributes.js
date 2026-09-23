2253089cf2fdf710f30ce0f084ef561186075a48
pepakurahuto-svg <pepakurahuto@gmail.com>
2026-09-24 00:19:04 +0900

    ver1.0.1
    キャラ作成の発動抵抗の必要経験値調整
    四属性バトル機能を実装
    バトル経験値の実装
    見た目の調整

--- attributes.js
+++ attributes.js
@@ -8,6 +8,7 @@ const MAGIC_ATTRIBUTES = {
 
 // 属性カラー
 const MAGIC_ATTRIBUTE_COLORS = {
+  0: "#e8ddff", // 全：やわらかい紫
   1: "#ffdddd", // 火：薄い赤
   2: "#ddeeff", // 水：薄い青
   3: "#ddffdd", // 風：薄い緑
@@ -16,6 +17,7 @@ const MAGIC_ATTRIBUTE_COLORS = {
 
 // 属性チャンピオン
 const MAGIC_ATTRIBUTE_CHAMPION = {
+  0: "魔導覇者", // 全属性
   1: "紅炎覇者", // 火
   2: "蒼水覇者", // 水
   3: "翠風覇者", // 風
@@ -24,6 +26,7 @@ const MAGIC_ATTRIBUTE_CHAMPION = {
 
 // 属性最大威力
 const MAGIC_ATTRIBUTE_MAXDAMAGE = {
+  0: "魔導王", // 全属性
   1: "炎壊王", // 火
   2: "水壊王", // 水
   3: "風壊王", // 風
