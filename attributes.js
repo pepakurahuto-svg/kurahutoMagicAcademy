52ab84845f866083b11bd6372591c830a919b3d4
pepakurahuto-svg <pepakurahuto@gmail.com>
2026-09-23 11:32:45 +0900

    ver1.0.0
    基本機能の公開

---
+++ attributes.js
@@ -0,0 +1,31 @@
+// 属性
+const MAGIC_ATTRIBUTES = {
+  1: "火",
+  2: "水",
+  3: "風",
+  4: "土"
+};
+
+// 属性カラー
+const MAGIC_ATTRIBUTE_COLORS = {
+  1: "#ffdddd", // 火：薄い赤
+  2: "#ddeeff", // 水：薄い青
+  3: "#ddffdd", // 風：薄い緑
+  4: "#f5e5cc"  // 土：薄い茶色
+};
+
+// 属性チャンピオン
+const MAGIC_ATTRIBUTE_CHAMPION = {
+  1: "紅炎覇者", // 火
+  2: "蒼水覇者", // 水
+  3: "翠風覇者", // 風
+  4: "黄土覇者"  // 土
+};
+
+// 属性最大威力
+const MAGIC_ATTRIBUTE_MAXDAMAGE = {
+  1: "炎壊王", // 火
+  2: "水壊王", // 水
+  3: "風壊王", // 風
+  4: "土壊王"  // 土
+};
